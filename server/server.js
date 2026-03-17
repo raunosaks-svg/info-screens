a99135703f339700451382ef8c63b90145289010
raunosaks <raunosaks@gmail.com>
2026-03-17 20:41:37 +0200

    Implement race state and realtime updates with Socket.IO

--- server/server.js
+++ server/server.js
@@ -13,38 +13,47 @@ app.use(express.static(path.join(__dirname, "../public"))); // Serve static file
 
 // ROUTES
 
-app.get("/front-desk", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/front-desk.html"));
-});
+const screens = [
+    "front-desk",
+    "race-control",
+    "lap-line-tracker",
+    "leader-board",
+    "next-race",
+    "race-countdown",
+    "race-flags"
+];
 
-app.get("/race-control", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/race-control.html"));
-});
-
-app.get("/lap-line-tracker", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/lap-line-tracker.html"));
-});
-
-app.get("/leader-board", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/leader-board.html"));
-});
-
-app.get("/next-race", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/next-race.html")); 
-});
-
-app.get("/race-countdown", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/race-countdown.html"));
-});
-
-app.get("/race-flags", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/race-flags.html"));
+screens.forEach((screen) => {
+    app.get(`/${screen}`, (req, res) => {
+        res.sendFile(path.join(__dirname, `../public/${screen}.html`));
+    });
 });
 
 // SOCKET.IO
 
+let raceState = {
+    status: "waiting", // waiting | countdown | racing | finished
+    drivers: [],
+    leaderboard: [],
+    countdown: 10,
+    currentLap: 0,
+    totalLaps: 10,
+    fastestLap: null
+};
+
 io.on("connection", (socket) => {
-    console.log("Client connected"); // Log when a client connects
+    console.log("Client connected");
+
+    // saada kohe kogu race state
+    socket.emit("raceState", raceState);
+
+    // update race state (nt race-control UI-st)
+    socket.on("updateRace", (data) => {
+        raceState = { ...raceState, ...data };
+
+        // saada update kõigile
+        io.emit("raceState", raceState);
+    });
 
     socket.on("disconnect", () => {
         console.log("Client disconnected");
